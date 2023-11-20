24679b64dbcf1fd018b7c0842cd6eaeb5f236a4b
gabriel lopes <gabriellopes112009@gmail.com>
2023-11-20 18:39:24 -0300

    Nova rota para criar jogos usando post e retornando página com a listagem

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 class Jogo:
 
@@ -7,13 +8,29 @@ class Jogo:
         self.categoria = categoraria
         self.console = console
 
+
 app = Flask(__name__)
 
+lista_jogos = [Jogo('tetris', 'puzzle', 'atari'),
+               Jogo('crash', 'aventura', 'ps1')]
 
 @app.route('/inicio')
-def ola():
-    lista_jogos = [Jogo('tetris','puzzle','atari'),
-                   Jogo('crash','aventura','ps1')]
-    return render_template('lista.html',titulo='Jogos', jogos = lista_jogos)
+def inicio():
+    return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
+
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
+@app.route('/criar',methods=['POST'])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista_jogos.append(jogo)
+    return inicio()
 
 app.run(port=8080)
