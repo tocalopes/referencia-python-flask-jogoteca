8da35c9a75c5fdf7b6db6fc99f83646f7acca42c
gabriel lopes <gabriellopes112009@gmail.com>
2023-11-20 18:18:37 -0300

    passagem de classes para diretiva for

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 
+class Jogo:
+
+    def __init__(self, nome, categoraria, console):
+        self.nome = nome
+        self.categoria = categoraria
+        self.console = console
+
 app = Flask(__name__)
 
 
 @app.route('/inicio')
 def ola():
-    lista_jogos = ['tetris', 'crash', 'vavazin de lei']
+    lista_jogos = [Jogo('tetris','puzzle','atari'),
+                   Jogo('crash','aventura','ps1')]
     return render_template('lista.html',titulo='Jogos', jogos = lista_jogos)
 
 app.run(port=8080)
